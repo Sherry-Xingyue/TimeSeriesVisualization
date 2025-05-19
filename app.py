c7dc64f994feed1b726b7a58dc60243a982fdcf8
Sherry-Xingyue <yuanxingyue@cuhk.edu.cn>
2025-05-19 16:26:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from itertools import combinations
 st.set_page_config(layout="wide")
 
 # 读取数据
-df = pd.read_csv("df_standardized.csv", index_col=0)
+df = pd.read_csv("df_standardized_finance.csv", index_col=0)
+#df = pd.read_csv("df_standardized.csv", index_col=0)
 df.index = pd.to_datetime(df.index)
 columns = df.columns.tolist()
 time_unit = 'month'
